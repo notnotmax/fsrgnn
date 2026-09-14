77b87de0ee8b2ef233005662d405a0d12e873651
notnotmax <e1129808@u.nus.edu>
2026-09-14 18:54:42 +0800

    add utils

---
+++ utils/file_utils.py
@@ -0,0 +1,73 @@
+import geopandas as gpd
+import h5py
+import numpy as np
+import os
+import pickle
+import yaml
+import shutil
+
+from typing import Any, List, Union
+
+def read_yaml_file(filepath: str) -> dict:
+    with open(filepath, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
+def save_to_yaml_file(filepath: str, data: dict):
+    if not os.path.exists(os.path.dirname(filepath)):
+        os.makedirs(os.path.dirname(filepath))
+
+    with open(filepath, 'w') as file:
+        yaml.dump(data, file)
+
+def read_shp_file_as_numpy(filepath: str, columns: str | list) -> np.ndarray:
+    file = gpd.read_file(filepath)
+    np_data = file[columns].to_numpy()
+    return np_data
+
+def read_hdf_file_as_numpy(filepath: str, property_path: str, separator: str = '.') -> np.ndarray:
+    with h5py.File(filepath, 'r') as hec:
+        data = get_property_from_path(hec, property_path, separator)
+        np_data = np.array(data)
+    return np_data
+
+def get_property_from_path(dict: dict, dict_path: str, separator: str = '.') -> Any:
+    keys = dict_path.split(sep=separator)
+    d = dict
+    for key in keys:
+        if key in d:
+            d = d[key]
+        else:
+            raise KeyError(f'Key {key} not found in dictionary for path {dict_path}')
+    return d
+
+def read_pickle_file(filepath: str) -> Any:
+    with open(filepath, 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+def save_to_pickle_file(filepath: str, data: Any):
+    if not os.path.exists(os.path.dirname(filepath)):
+        os.makedirs(os.path.dirname(filepath))
+
+    with open(filepath, 'wb') as file:
+        pickle.dump(data, file)
+
+def create_temp_dirs(paths: Union[List[str], str], folder_name: str = '_temp_dir') -> List[str]:
+    if isinstance(paths, str):
+        paths = [paths]
+
+    temp_dir_paths = []
+    for path in paths:
+        temp_dir_path = os.path.join(path, folder_name)
+        if not os.path.exists(temp_dir_path):
+            os.makedirs(temp_dir_path)
+        temp_dir_paths.append(temp_dir_path)
+    return temp_dir_paths
+
+def delete_temp_dirs(paths: Union[List[str], str]):
+    if isinstance(paths, str):
+        paths = [paths]
+    for path in paths:
+        if os.path.exists(path):
+            shutil.rmtree(path)
---
+++ utils/model_utils.py
@@ -0,0 +1,127 @@
+from torch import Tensor
+from torch.nn import Module, Sequential, Linear, PReLU, ReLU, \
+    MSELoss, L1Loss, HuberLoss, LayerNorm
+from torch_geometric.nn import GCNConv, SAGEConv, GINConv, Sequential as PygSequential
+
+def make_mlp(
+    input_size: int,
+    output_size: int,
+    hidden_size: int = None,
+    num_layers: int = 1,
+    activation: str = None,
+    norm: str = None,
+    bias: bool = True,
+    device: str = 'cpu') -> Module:
+    if num_layers == 1:
+        layers = [LinearLayer(input_size, output_size, activation, bias, device)]
+    else:
+        hidden_size = hidden_size if hidden_size is not None else (input_size * 2)
+        layers = []
+        layers.append(LinearLayer(input_size, hidden_size, activation, bias, device)) # Input Layer
+        for _ in range(num_layers-2):
+            layers.append(LinearLayer(hidden_size, hidden_size, activation, bias, device)) # Hidden Layers
+        layers.append(LinearLayer(hidden_size, output_size, None, bias, device)) # Output Layer
+
+    if norm is not None:
+        norm_layer = get_norm_layer(norm, output_size, device=device)
+        layers.append(norm_layer)
+
+    return Sequential(*layers) if len(layers) > 1 else layers[0]
+
+def make_gnn(input_size: int, output_size: int, hidden_size: int = None,
+             num_layers: int = 1, conv: str = 'gcn',
+             activation: str = None, device: str = 'cpu', **conv_kwargs) -> Module:
+    if num_layers == 1:
+        return GNNLayer(input_size, output_size, conv, activation, device, **conv_kwargs)
+
+    hidden_size = hidden_size if hidden_size is not None else (input_size * 2)
+    layers = []
+    layers.append(
+        (GNNLayer(input_size, hidden_size, conv, activation, device, **conv_kwargs), 'x, edge_index -> x')
+    ) # Input Layer
+    for _ in range(num_layers-2):
+        layers.append(
+            (GNNLayer(hidden_size, hidden_size, conv, activation, device, **conv_kwargs), 'x, edge_index -> x')
+        ) # Hidden Layers
+    layers.append(
+        (GNNLayer(hidden_size, output_size, conv, None, device, **conv_kwargs), 'x, edge_index -> x')
+    ) # Output Layer
+    return PygSequential('x, edge_index', layers)
+
+def get_activation_func(name: str, device: str = 'cpu') -> Module:
+    if name == 'relu':
+        return ReLU()
+    if name == 'prelu':
+        return PReLU(device=device)
+    raise Exception(f'Activation function {name} is not implemented.')
+
+def get_loss_func(name: str, **loss_func_params) -> Module:
+    if name == 'mse':
+        return MSELoss(**loss_func_params)
+    if name == 'mae':
+        return L1Loss(**loss_func_params)
+    if name == 'huber':
+        return HuberLoss(**loss_func_params)
+    raise Exception(f'Loss function {name} is not implemented.')
+
+def get_norm_layer(name: str, num_features: int, device: str = 'cpu') -> Module:
+    if name == 'layernorm':
+        return LayerNorm(num_features).to(device)
+    raise Exception(f'Normalization layer {name} is not implemented.')
+
+class LinearLayer(Module):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        activation: str = None,
+        bias: bool = True,
+        device: str = 'cpu'):
+        super().__init__()
+        self.in_features = in_features # For GINE compatibility
+        self.out_features = out_features
+        self.linear = Linear(in_features=in_features, out_features=out_features, bias=bias, device=device)
+        if activation is not None:
+            self.activation = get_activation_func(activation, device=device)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.linear(x)
+        if hasattr(self, 'activation'):
+            x = self.activation(x)
+        return x
+
+    def reset_parameters(self):
+        self.linear.reset_parameters()
+        if hasattr(self, 'activation') and hasattr(self.activation, 'reset_parameters'):
+            self.activation.reset_parameters()
+
+class GNNLayer(Module):
+    def __init__(self,
+                 in_features: int,
+                 out_features: int,
+                 conv: str = 'gcn',
+                 activation: str = None,
+                 device: str = 'cpu',
+                 **conv_kwargs):
+        super().__init__()
+        self.conv = self._get_conv(conv, in_features, out_features, **conv_kwargs).to(device)
+        if activation is not None:
+            self.activation = get_activation_func(activation, device=device)
+
+    def _get_conv(self, conv: str, in_features: int, out_features: int, **conv_kwargs) -> Module:
+        if conv == 'gcn':
+            return GCNConv(in_channels=in_features, out_channels=out_features, **conv_kwargs)
+        if conv == 'sage':
+            return SAGEConv(in_channels=in_features, out_channels=out_features, **conv_kwargs)
+        raise Exception(f'GNN Convolution {conv} is not implemented.')
+
+    def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
+        x = self.conv(x, edge_index)
+        if hasattr(self, 'activation'):
+            x = self.activation(x)
+        return x
+
+    def reset_parameters(self):
+        self.conv.reset_parameters()
+        if hasattr(self, 'activation') and hasattr(self.activation, 'reset_parameters'):
+            self.activation.reset_parameters()
